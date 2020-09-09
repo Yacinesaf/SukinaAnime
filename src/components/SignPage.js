6ff25a08f238579361ec0fe64e46fd41149de504
Yacinesaf <yacinesaf96@gmail.com>
2020-09-09 18:02:26 -0400

    signpage is done in mobile

--- src/components/SignPage.js
+++ src/components/SignPage.js
@@ -14,13 +14,13 @@ export default class SignPage extends Component {
   }
   render() {
     return (
-      <div className='row justify-content-center mx-0 px-0'>
-        <div className='col-xs-11 col-md-6 d-flex align-items-center' style={{ padding: this.props.smDown ? 0 : '0px 100px' }}>
+      <div className='row justify-content-center mx-0 px-0' style={{height : this.props.smDown ? '100vh' : 'unset'}}>
+        <div className='col-10 col-md-6 d-flex align-items-center' style={{ padding: this.props.smDown ? 0 : '0px 100px' }}>
           <form style={{ width: 'inherit' }}>
-            <label htmlFor='email' className='py-3 field-title'>Email adress</label>
+            <label htmlFor='email' className='py-1 py-md-3 field-title'>Email adress</label>
             <input id='email' className='inputfield' type='text' placeholder='Enter Email' />
-            <label htmlFor='password' className='py-3 field-title'>Password</label>
-            <input id='password' className='inputfield mb-4' type='password' placeholder='Password' />
+            <label htmlFor='password' className='py-1 py-md-3 field-title'>Password</label>
+            <input id='password' className='inputfield mb-2 mb-md-4' type='password' placeholder='Password' />
             <div className='button mt-4'>
               {this.props.location.pathname === 'login' ? 'Login' : 'Sign up'}
             </div>
